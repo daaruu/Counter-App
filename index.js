79842e8af188538a766e67b75adf9c44ff790244
Daru <sachansujal773@gmail.com>
2024-02-03 00:11:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let count = 0;
+
+document.getElementById("count").textContent = count;
+
+document.getElementById("inc").onclick = function (){
+    count++;
+    document.getElementById("count").textContent = count;
+}
+
+document.getElementById("dec").onclick = function (){
+    count--;
+    document.getElementById("count").textContent = count;
+}
+
+document.getElementById("res").onclick = function (){
+    count = 0;
+    document.getElementById("count").textContent = count;
+}
